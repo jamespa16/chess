4bd405b2e9d6751f23189e826169e394dd280c8e
James Palmer <james.pa@outlook.com>
2026-01-21 19:08:05 -0700

    Implemented ChessBoard according to tests/scaffolding

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Arrays;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,9 +9,14 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    ChessPiece[][] board = new ChessPiece[8][8];
 
     public ChessBoard() {
-        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; i < board[0].length; i++) {
+                board[i][j] = null;
+            }
+        }
     }
 
     /**
@@ -19,7 +26,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     /**
@@ -30,7 +37,12 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        int row = position.getRow();
+        int col = position.getColumn();
+        if (row < 9 && row > 0 && col < 9 && col > 0) {
+            return board[row - 1][col - 1];
+        }
+        return null;
     }
 
     /**
@@ -38,6 +50,67 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; i < board[0].length; i++) {
+                board[i][j] = null;
+            }
+        }
+
+        addBackRowHelper(ChessGame.TeamColor.WHITE);
+        addBackRowHelper(ChessGame.TeamColor.BLACK);
+
+        for (int i = 0; i < board[1].length; i++) {
+            addPiece(new ChessPosition(2, i + 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+        }
+
+        for (int i = 0; i < board[6].length; i++) {
+            addPiece(new ChessPosition(7, i + 1), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+        }
+    }
+
+    private void addBackRowHelper(ChessGame.TeamColor color) {
+        int row;
+
+        if (color == ChessGame.TeamColor.WHITE) {
+            row = 1;
+        } else {
+            row = 8;
+        }
+
+        addPiece(new ChessPosition(row, 1), new ChessPiece(color, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(row, 2), new ChessPiece(color, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(row, 3), new ChessPiece(color, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(row, 4), new ChessPiece(color, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(row, 5), new ChessPiece(color, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(row, 6), new ChessPiece(color, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(row, 7), new ChessPiece(color, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(row, 8), new ChessPiece(color, ChessPiece.PieceType.ROOK));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ChessBoard that)) {
+            return false;
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] != null && that.board[i][j] != null) {
+                    if(board[i][j].getPieceType() != that.board[i][j].getPieceType()) {
+                        return false;
+                    }
+                } else if (board[i][j] == null && that.board[i][j] != null) {
+                    return false;
+                } else if (board[i][j] != null && that.board[i][j] == null) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
     }
 }
