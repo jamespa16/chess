85cf1d25f3c40bbdfe92e8d77b22e1d135cc9a43
James Palmer <james.pa@outlook.com>
2026-02-26 16:45:17 -0700

    added service tests file & registered HTTP endpoints on server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,24 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
 
 public class Server {
 
     private final Javalin javalin;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
-
-        // Register your endpoints and exception handlers here.
-
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+                .post("/user", this::registerUser)
+                .post("/session", this::loginUser)
+                .delete("/session", this::logoutUser)
+                .get("/game", this::listGames)
+                .post("/game", this::newGame)
+                .put("/game", this::joinGame)
+                .delete("/db", this::clearDatabase);
     }
 
     public int run(int desiredPort) {
@@ -21,4 +29,12 @@ public class Server {
     public void stop() {
         javalin.stop();
     }
+
+    public void registerUser(Context ctx) {}
+    public void loginUser(Context ctx) {}
+    public void logoutUser(Context ctx) {}
+    public void listGames(Context ctx) {}
+    public void newGame(Context ctx) {}
+    public void joinGame(Context ctx) {}
+    public void clearDatabase(Context ctx) {}
 }
